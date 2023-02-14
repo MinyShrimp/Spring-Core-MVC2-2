a2590b1e5315537e52bd5a010e12967e7aa0633b
MinyShrimp <ksk7584@gmail.com>
2023-02-14 23:16:38 +0900

    4강: 검증 1 - Validation - 프로젝트 설정 V1

--- src/main/java/hello/springcoremvc22/TestDataInit.java
+++ src/main/java/hello/springcoremvc22/TestDataInit.java
@@ -1,7 +1,7 @@
 package hello.springcoremvc22;
 
-import hello.springcoremvc22.domain.item.Item;
-import hello.springcoremvc22.domain.item.ItemRepository;
+import hello.springcoremvc22.domain.validation.Item;
+import hello.springcoremvc22.domain.validation.ItemRepository;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class TestDataInit {
     private final ItemRepository itemRepository;
-
+    
     @PostConstruct
     public void init() {
         itemRepository.save(new Item("itemA", 1000, 10));
---
+++ src/main/java/hello/springcoremvc22/web/validation/ValidationItemControllerV1.java
@@ -0,0 +1,73 @@
+package hello.springcoremvc22.web.validation;
+
+import hello.springcoremvc22.domain.validation.Item;
+import hello.springcoremvc22.domain.validation.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
+
+@Slf4j
+@Controller
+@RequestMapping("/validation/v1/items")
+@RequiredArgsConstructor
+public class ValidationItemControllerV1 {
+    private final ItemRepository itemRepository;
+
+    @GetMapping
+    public String items(Model model) {
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "validation/v1/items";
+    }
+
+    @GetMapping("/{itemId}")
+    public String item(
+            @PathVariable long itemId,
+            Model model
+    ) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "validation/v1/item";
+    }
+
+    @GetMapping("/add")
+    public String addForm(Model model) {
+        model.addAttribute("item", new Item());
+        return "validation/v1/addForm";
+    }
+
+    @PostMapping("/add")
+    public String addItem(
+            @ModelAttribute Item item,
+            RedirectAttributes redirectAttributes
+    ) {
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v1/items/{itemId}";
+    }
+
+    @GetMapping("/{itemId}/edit")
+    public String editForm(
+            @PathVariable long itemId,
+            Model model
+    ) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "validation/v1/editForm";
+    }
+
+    @PostMapping("/{itemId}/edit")
+    public String edit(
+            @PathVariable long itemId,
+            @ModelAttribute Item item
+    ) {
+        itemRepository.update(itemId, item);
+        return "redirect:/validation/v1/items/{itemId}";
+    }
+}
